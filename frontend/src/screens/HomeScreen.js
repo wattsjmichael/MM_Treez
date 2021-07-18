a0ede2f7585f67e5d6382ffc8f76b5f12fe15e4b
Michael Watts <wattsjmichael@gmail.com>
2021-07-18 11:15:47 -0700

    adds reducer for ProductList

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,31 +1,36 @@
-import React, {useState, useEffect} from 'react'
+import React, { useEffect } from 'react'
 import { Row, Col } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
 import Product from '../components/Product'
-import axios from 'axios'
+import Message from '../components/Message'
+import Loader from '../components/Loader'
+import { listProducts } from '../actions/productActions'
+
 
 const HomeScreen = () => {
-const [products, setProducts] = useState([])
+  const dispatch = useDispatch()
 
-useEffect(()=>{
-  const fetchProducts = async () => {
-    const {data} = await axios.get('/api/products')
+  const productList = useSelector(state => state.productList)
+  const { loading, error, products } = productList
 
-    setProducts(data)
-  }
-  
-  fetchProducts()
-}, [])
+
+  useEffect(() => {
+    dispatch(listProducts())
+
+  }, [dispatch])
+
+
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) =>(
+      {loading ? ( <Loader /> ) : error ? ( <Message variant='danger'>{error}</Message>) : <Row>
+        {products.map((product) => (
           <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-          <Product product={product} />
-          </Col>   
+            <Product product={product} />
+          </Col>
         ))}
-      </Row>
+      </Row>}
     </>
   )
 }
