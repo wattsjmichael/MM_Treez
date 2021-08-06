4970b0627d620b5019404b1153d66ee26e9dc4c2
Michael Watts <wattsjmichael@gmail.com>
2021-08-06 08:53:11 -0700

    adds User Action, Reducer, and constants

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -25,6 +25,38 @@ const authUser = asyncHandler(async (req, res) => {
   }
 })
 
+// @ Desc Register a new user
+// @Route Post /api/user
+// @access Public Route
+const registerUser = asyncHandler(async (req, res) => {
+  const { name, email, password } = req.body
+
+  const userExists = await User.findOne({ email })
+  if (userExists){
+    res.status(400)
+    throw new Error("User already exists")  
+  }
+
+  const user = await User.create({
+    name,
+    email,
+    password
+  })
+
+  if(user){
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+      isAdmin: user.isAdmin,
+      token: generateToken(user._id),
+    })
+  } else {
+    res.status(400)
+    throw new Error('Invalid user data')
+  }
+})
+
 // @Desc Get user profile
 // @Route GET /api/user/profile
 // @access Private
@@ -49,4 +81,4 @@ const getUserProfile = asyncHandler(async (req, res) => {
 
 
 
-export { authUser, getUserProfile }
+export { authUser, getUserProfile, registerUser }
